6e17f2a6b4d26fd548848caed0e611057644011e
nghialt3670 <nghialt3670@gmail.com>
2025-06-10 16:50:23 +0700

    Initial commit

---
+++ src/main/java/com/funchive/gatewayserver/FunchiveGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.funchive.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunchiveGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunchiveGatewayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funchive/gatewayserver/FunchiveGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funchive.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunchiveGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
